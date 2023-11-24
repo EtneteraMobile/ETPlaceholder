4ee14852c7dcd9435272d96a2562a74e984eabab
Artem Kandaurov <artem@kandaurov.net>
2023-11-24 03:04:09 +0100

    Specify sources

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ETPlaceholder",
-            targets: ["ETPlaceholder"]),
+            targets: ["ETPlaceholder"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ETPlaceholder",
-            dependencies: []),
-        .testTarget(
-            name: "ETPlaceholderTests",
-            dependencies: ["ETPlaceholder"]),
+            dependencies: [],
+            path: "Sources"
+        ),
     ]
 )
